5182082248812b088ffc6e91418efa69fb3b6f1e
tomek7777 <89088480+tomek7777@users.noreply.github.com>
2022-12-02 17:55:07 +0100

    slider upgrade - done

--- lab 02 slider/main.js
+++ lab 02 slider/main.js
@@ -1,24 +1,43 @@
-// notatnik z zajęć
-const main = document.querySelector('main')
-const timeoutRef = setTimeout( 
-    () => {
-        main.innerHTML='From setTimeout'
-    },
-    2000
-)
-let licznik = 0 
-const intervalRef = setInterval( 
-    () => {
-        main.innerHTML='From interval' + licznik++
-    },
-    4000
-)
-
-// kasujemy setInterval
-clearInterval(intervalRef)
-
-// kasujemy setTimeout
-clearTimeout(intervalRef)
+let slideIndex = 1;
+showSlides(slideIndex);
 
 
-// window.requestAnimationFrame
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+let autoSlideIndex = 0;
+autoShowSlides();
+
+function autoShowSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(autoShowSlides, 2000); // 2 seconds
+}
